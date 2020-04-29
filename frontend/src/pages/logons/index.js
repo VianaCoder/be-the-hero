619851c2caecbf0e5dc4fd7baa1eef4e1021ddbc
Gabriel Viana <4463gabriel@gmail.com>
2020-04-29 00:33:02 -0300

    Front 0.1

--- frontend/src/pages/logons/index.js
+++ frontend/src/pages/logons/index.js
@@ -17,7 +17,7 @@ function Logon() {
 
                     <input placeholder="Sua ID" />
 
-                    <button type="submit">Entrar</button>
+                    <button className="button" type="submit">Entrar</button>
 
                     <a href="/register">
                     <FiLogIn size={16} color="#e02041" />
