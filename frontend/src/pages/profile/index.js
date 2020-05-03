add4b63962a663ff12dc34d697dabb3e37f2b995
Gabriel Viana <4463gabriel@gmail.com>
2020-05-02 22:24:02 -0300

    front 0.5

---
+++ frontend/src/pages/profile/index.js
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import './style.css'
+import { Link } from 'react-router-dom';
+
+import logoImg from  '../../assets/logo.svg';
+import { FiPower, FiTrash } from 'react-icons/fi'
+
+function Profile() {
+
+    return(
+        <div className="profile_block">
+            <header>
+                <section className="left">
+                <img src={logoImg} alt="Be the Hero"/>
+                <span>Bem vinda, APAD</span>
+                </section>
+
+                <section className="right">
+                <Link to="/incidents/new" className="button">Cadastrar Novo Caso</Link>
+                <button><FiPower size={24} color="#E02041" className="power"/></button>
+                </section>
+            </header>
+
+            <h1>Casos Cadastrados:</h1>
+            <ul>
+                <li>
+                    <strong>CASO:</strong>
+                    <p>Nome do Caso</p>
+
+                    <strong>DESCRIÇÃO:</strong>
+                    <p>Descriçao do Caso</p>
+
+                    <strong>VALOR:</strong>
+                    <p>R$ 120,00</p>
+                    <button><FiTrash size={18} className="delete"/></button>
+                </li>
+
+                <li>
+                    <strong>CASO:</strong>
+                    <p>Nome do Caso</p>
+
+                    <strong>DESCRIÇÃO:</strong>
+                    <p>Descriçao do Caso</p>
+
+                    <strong>VALOR:</strong>
+                    <p>R$ 120,00</p>
+                    <button><FiTrash size={18} className="delete"/></button>
+                </li>
+
+                <li>
+                    <strong>CASO:</strong>
+                    <p>Nome do Caso</p>
+
+                    <strong>DESCRIÇÃO:</strong>
+                    <p>Descriçao do Caso</p>
+
+                    <strong>VALOR:</strong>
+                    <p>R$ 120,00</p>
+                    <button><FiTrash size={18} className="delete"/></button>
+                </li>
+
+                <li>
+                    <strong>CASO:</strong>
+                    <p>Nome do Caso</p>
+
+                    <strong>DESCRIÇÃO:</strong>
+                    <p>Descriçao do Caso</p>
+
+                    <strong>VALOR:</strong>
+                    <p>R$ 120,00</p>
+                    <button><FiTrash size={18} className="delete"/></button>
+                </li>
+            </ul>
+
+
+
+
+        </div>
+    );
+}
+
+export default Profile;
